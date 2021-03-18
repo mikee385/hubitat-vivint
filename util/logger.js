748353de98c25c7670edea53e3ee40e73439ac60
Michael Pierce <mikee385@gmail.com>
2021-03-18 16:34:47 -0500

    Improve the logging format

--- util/logger.js
+++ util/logger.js
@@ -1,33 +1,57 @@
 var winston = require('winston')
 
+const commonFormat = winston.format.combine(
+    winston.format.metadata(),
+    winston.format.timestamp({format: 'YYYY-MM-DD HH:mm:ss'}),
+    winston.format.printf((info) => {
+        let text = ""
+    
+        if (info.timestamp) {
+            text += `${info.timestamp}\t`
+        }
+    
+        text += `${info.level}:`
+    
+        if (typeof info.message === 'string' || info.message instanceof String) {
+            text += `\t${info.message.trim()}`
+        } else {
+            text += `\t${info.message}`
+        }
+        
+        if (info.metadata && Object.keys(info.metadata).length) {
+            if (info.metadata.stack) {
+                text += `\n${info.metadata.stack}`
+            } else {
+                text += ` ${JSON.stringify(info.metadata, undefined, 4)}`
+            }
+        }
+    
+        if (info.stack) {
+            text += `\n${info.stack}`
+        }
+    
+        return text
+    })
+)
+
 var logger = winston.createLogger({
     transports: [
         new winston.transports.Console({
             level: 'info',
             format: winston.format.combine(
                 winston.format.colorize(),
-                winston.format.metadata(),
-                winston.format.timestamp({format: 'YYYY-MM-DD HH:mm:ss:ms'}),
-                winston.format.printf((info) => `${info.timestamp} ${info.level}: ${info.message.trim()} ${Object.keys(info.metadata).length ? JSON.stringify(info.metadata, undefined, 4) : ''}`)
-            ),
+                commonFormat
+            )
         }),
         new winston.transports.File({
             filename: './logs/debug.log',
             level: 'debug',
-            format: winston.format.combine(
-                winston.format.metadata(),
-                winston.format.timestamp({format: 'YYYY-MM-DD HH:mm:ss:ms'}),
-                winston.format.printf((info) => `${info.timestamp} ${info.level}: ${info.message.trim()} ${Object.keys(info.metadata).length ? JSON.stringify(info.metadata, undefined, 4) : ''}`)
-            ),
+            format: commonFormat
         }),
         new winston.transports.File({
             filename: './logs/error.log',
             level: 'warn',
-            format: winston.format.combine(
-                winston.format.metadata(),
-                winston.format.timestamp({format: 'YYYY-MM-DD HH:mm:ss:ms'}),
-                winston.format.printf((info) => `${info.timestamp} ${info.level}: ${info.message.trim()} ${Object.keys(info.metadata).length ? JSON.stringify(info.metadata, undefined, 4) : ''}`)
-            ),
+            format: commonFormat
         })
     ]
 })
