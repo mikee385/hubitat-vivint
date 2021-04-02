51be1365a5e01ffb614e06e6f593b8814c2d93ff
Michael Pierce <mikee385@gmail.com>
2021-04-02 11:36:19 -0500

    Add psuedo-battery level when BatteryLevel is missing but LowBattery is present

--- nodejs/lib/device.js
+++ nodejs/lib/device.js
@@ -21,6 +21,10 @@ class Device {
 
       if (!Object.is(data.BatteryLevel, undefined)) {
         this.battery = data.BatteryLevel
+      } else if (!Object.is(data.LowBattery, undefined)) {
+        this.battery = Boolean(data.LowBattery)
+          ?  10 // Use 10% for low battery
+          : 100 // Use 100% for not low battery
       }
 
       if (!Object.is(data.Tamper, undefined)) {
